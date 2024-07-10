c27479867f1ea14f8e43bb970177a6df1c76ca1f
Sidharth Arya <code@sidhartharya.com>
2024-07-11 04:48:15 +0530

    Module Echo remove

--- modules.sh
+++ modules.sh
@@ -1,12 +1,12 @@
 m () {
   ARG1=$1
   ARG2=$2
-  echo $ARG1 $ARG2
+  # echo $ARG1 $ARG2
   if [[ $ARG1 == "a" ]]; then
-    echo Activating $ARG2
+    # echo Activating $ARG2
     source ~/.config/zsh/modules/$ARG2 activate
     elif [[ $ARG1 == "d" ]]; then
-    echo Deactivating $ARG2
+    # echo Deactivating $ARG2
     source ~/.config/zsh/modules/$ARG2 deactivate
     else
     echo "Usage: m [a|d] [module]"
