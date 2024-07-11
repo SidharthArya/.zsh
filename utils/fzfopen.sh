f8f51985840c9df58bdc44d876a5b527e4bd9cfd
Sidharth Arya <code@sidhartharya.com>
2024-07-11 07:19:03 +0530

    fzf helper

---
+++ utils/fzfopen.sh
@@ -0,0 +1,15 @@
+FZF=fzf
+
+OUT='inode/directory'
+PT='.'
+while [[ $OUT == 'inode/directory' ]]; 
+do
+  cd $PT;
+  FNAME=$(find -maxdepth 5 -type f,d,s | fzf)
+  OUT=$(file --mime-type $FNAME | awk '{print $2}');
+  
+done
+
+if [[ $(printf %.4s $OUT) == 'text' ]]; then
+  nvim $FNAME
+fi
