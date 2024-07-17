3e4ba43033f92744dd0d88abc0d6a3f3c78a9477
Sidharth Arya <code@sidhartharya.com>
2024-07-18 03:43:15 +0530

    [quick open] fzf

--- utils/fzfopen.sh
+++ utils/fzfopen.sh
@@ -7,9 +7,8 @@ do
   cd $PT;
   FNAME=$(find -maxdepth 5 -type f,d,s | fzf)
   OUT=$(file --mime-type $FNAME | awk '{print $2}');
-  
+  PT=$FNAME;
 done
-
-if [[ $(printf %.4s $OUT) == 'text' ]]; then
+# if [[ $(printf %.4s $OUT) == 'text' ]]; then
   nvim $FNAME
-fi
+# fi
